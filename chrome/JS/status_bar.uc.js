8cb323f55c6e95f603f221a0f91cd9e4a5e06b23
travis <94665268+travy-patty@users.noreply.github.com>
2023-07-27 16:38:19 -0700

    add fuckerfox

---
+++ chrome/JS/status_bar.uc.js
@@ -0,0 +1,23 @@
+let status = document.createElement("vbox");
+status.setAttribute("id", "browser-bottombox");
+status.innerHTML = `
+<div class="statusBarInner">
+	<div class="statusBarIcon"></div>
+	<div class="statusBarDone">Done</div>
+</div>
+<div class="statusBarInner statusBarInnerTwoBoxes">
+</div>
+<div class="statusBarInner statusBarInnerTwoBoxes">
+</div>
+<div class="statusBarInner statusBarInnerTwoBoxes">
+</div>
+<div class="statusBarInner statusBarInnerTwoBoxes">
+</div>
+<div class="statusBarInner statusBarInnerTwoBoxes">
+</div>
+<div class="statusBarInternet">
+	<div class="statusBarIcon"></div>
+	<div class="statusBarDone">Internet</div>
+</div>
+`;
+document.body.appendChild(status);
